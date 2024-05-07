2a541d33127955cdd9612c0f1e541c8c425e56cd
Amir Golparvar <golparvar.amir@gmail.com>
2024-05-07 15:46:24 +0200

    added api_query

--- PromptGenerator.py
+++ PromptGenerator.py
@@ -17,9 +17,6 @@ prompt_starter = [
 ]
 
 config = dotenv_values(".env")
-client = OpenAI(
-    api_key=config['OPENAI_API_KEY'],
-)
 
 
 @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
