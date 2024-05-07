2935aefec627500b75ed4e135610f76f395f0af8
mohammad <mohammad.nakhaee.1@gmail.com>
2024-05-07 14:14:27 +0200

    human-readable prompts

--- PromptGenerator.py
+++ PromptGenerator.py
@@ -20,7 +20,7 @@ def random_date_generator(start_date, range_in_days):
 
 def get_raw_prompts(key):
     prompt_key = f'Based on the path "{key}" write 3 possible short exact enough human readable prompt and 3 short readable prompt containing abbreviation parameter that addresses this path. Just list them. Write it in the following format'
-    return ['I need a', 'I need b']
+    return ['I need key.a', 'I need key.b']
 
 
 # Load the YAML file
@@ -33,6 +33,16 @@ def make_quantity_query(key, val, query_type):
     query_key = f'{key}{query_type}'
     query[query_key] = val
     return query
+
+
+def list_to_text(items, sep):
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    else:
+        return ', '.join(items[:-1]) + f' {sep} ' + items[-1]
+
 
 # Loop over all keys and print them
 def print_keys(data, indent=''):
@@ -49,15 +59,19 @@ def print_keys(data, indent=''):
         if isinstance(type, list):
             values = type
         elif type == 'str':
-            values = [my_random_string(10), my_random_string(10)]
+            values = [my_random_string(10), my_random_string(10), my_random_string(10)]
         elif type == 'bool':
-            values = ['True', 'False']
+            values = ['True', 'False', 'True']
         elif type == 'int' or type == 'int32' or type == 'int64':
-            values = [str(random.randint(1000)), str(random.randint(1000))]
+            values = [str(random.randint(1000)), str(random.randint(1000)), str(random.randint(1000))]
         elif type == 'float' or type == 'float32' or type == 'float64':
-            values = ["{:.5f}".format(random.randint(10) * random.random()), "{:.5f}".format(random.randint(10) * random.random())]
+            values = ["{:.5f}".format(random.randint(10) * random.random()),
+                      "{:.5f}".format(random.randint(10) * random.random()),
+                      "{:.5f}".format(random.randint(10) * random.random())]
         elif type.startswith('<nomad.metainfo.metainfo._Datetime'):
-            values = [random_date_generator(f'2010-03-14T15:0{str(random.randint(9))}:20.36', 1000).astype(str), random_date_generator(f'2010-03-14T15:0{str(random.randint(9))}:32.21', 500).astype(str)]
+            values = [random_date_generator(f'2010-03-14T15:0{str(random.randint(9))}:20.36', 1000).astype(str),
+                      random_date_generator(f'2010-03-14T15:0{str(random.randint(9))}:32.21', 500).astype(str),
+                      random_date_generator(f'2010-03-14T15:0{str(random.randint(9))}:32.21', 500).astype(str)]
 
         raw_prompts = get_raw_prompts(key)
         if isinstance(values, list):
@@ -70,11 +84,11 @@ def print_keys(data, indent=''):
                             prompt = raw_prompt + f' with the value {val}'
                             quantity_query = make_quantity_query(key, val, query_type)
                         elif query_type == ':any' or query_type == ':all':
-                            multi_val = rnd.sample(values, random.randint(2, min(len(values), 3) + 1))
+                            multi_val = rnd.sample(values, random.randint(2, min(len(values), 4) + 1))
                             if query_type == ':any':
-                                prompt = raw_prompt + f' with one of the values {multi_val}'
+                                prompt = raw_prompt + f' that could be any of the {list_to_text(multi_val, "or")}'
                             else:
-                                prompt = raw_prompt + f' with all the values {multi_val}'
+                                prompt = raw_prompt + f' that includes all of the {list_to_text(multi_val, "and")}'
                             quantity_query = make_quantity_query(key, multi_val, query_type)
 
                         if quantity_query:
