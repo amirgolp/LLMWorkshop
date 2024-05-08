19529f59a03756d05e1a90278ac05e7d0c1b71d3
mohammad <mohammad.nakhaee.1@gmail.com>
2024-05-08 17:10:57 +0200

    shuffle all primary prompts

--- shuffle.py
+++ shuffle.py
@@ -13,57 +13,61 @@ def to_dict(input_file):
             completion = infile.readline().strip().replace("'", '"')
             if not completion:
                 break
-            prompt = dict(prompt=prompt, completion=json.loads(completion))
+            normalized_completion = completion.replace("True", "true").replace("False", "false")
+            prompt = dict(prompt=prompt, completion=json.loads(normalized_completion))
             all_prompts.append(prompt)
      
     return all_prompts
 
 
-def remove_starting_phrases(sentence):
-    patterns = [r'^I need the materials that\s+', r'^Give me the materials that\s+', r'^List the materials that\s+', r'^Search for the materials that\s+', r'^Look for the materials that\s+', r'^I need\s+', r'^Give me\s+', r'^List the\s+', r'^Search for\s+', r'^Look for\s+']
-    regex_patterns = [re.compile(pattern, re.IGNORECASE) for pattern in patterns]
-    for pattern in regex_patterns:
-        match = pattern.match(sentence)
-        if match:
-            sentence = sentence[match.end():].lstrip()
-            break
+starting_prompts = [
+    'Write a Nomad query.',
+    'Give me a Nomad query.',
+    'I need a Nomad query.',
+    'A Nomad query.',
+    'Nomad query.'
+]
 
-    return sentence
+n_total = 0
 
 
 def shuffle(primary_prompts, n, semantic):
     samples = random.sample(primary_prompts, n)
     prompts = [sample['prompt'] for sample in samples]
 
-    cleared_prompts = [remove_starting_phrases(prompts[i]) for i in range(1, len(prompts))]
-    cleared_prompts.insert(0, prompts[0])
-
     and_semantic = ['and', 'in which', 'considering that']
     or_semantic = ['or', 'and the materials that', 'or the materials that']
-    final_prompt = cleared_prompts[0]
-    for i in range(1, len(cleared_prompts)):
+    final_prompt = prompts[0]
+    for i in range(1, len(prompts)):
         if semantic == 'and':
-            final_prompt = f"{final_prompt} {random.sample(and_semantic, 1)[0]} {cleared_prompts[i]}"
+            final_prompt = f"{final_prompt} {random.sample(and_semantic, 1)[0]} {prompts[i]}"
         elif semantic == 'or':
-            final_prompt = f"{final_prompt} {random.sample(or_semantic, 1)[0]} {cleared_prompts[i]}"
+            final_prompt = f"{final_prompt} {random.sample(or_semantic, 1)[0]} {prompts[i]}"
     
     queries = [sample['completion'] for sample in samples]
     final_query = {semantic: {}}
     for d in queries:
         final_query[semantic].update(d)
-    final_dict = dict(prompt=final_prompt, completion=final_query)
+    final_dict = dict(prompt=f'{random.sample(starting_prompts, 1)[0]} {final_prompt}', completion=final_query)
     if len(final_query[semantic]) == n:
-        return final_dict
+        print(final_dict)
     else:
         return None
 
 
-primary_prompts = to_dict('version_1.txt')
+def make_final_prompts(primary_prompts):
+    for item in primary_prompts:
+        prompt = item['prompt']
+        query = item['completion']
+        final_dict = dict(prompt=f'{random.sample(starting_prompts, 1)[0]} {prompt}', completion=query)
+        print(final_dict)
 
-# prompts_level2 = [shuffle(primary_prompts, 2, 'and') for i in range(20)]
-# prompts_level3 = [shuffle(primary_prompts, 3, 'and') for i in range(5)]
-prompts_level4 = [shuffle(primary_prompts, 4, 'and') for i in range(5)]
 
-# print(prompts_level2)
-# print(prompts_level3)
-print(prompts_level4)
+primary_prompts = to_dict('version_8.txt')
+make_final_prompts(primary_prompts)
+and_prompts_level2 = [shuffle(primary_prompts, 2, 'and') for i in range(20000)]
+and_prompts_level3 = [shuffle(primary_prompts, 3, 'and') for i in range(20000)]
+and_prompts_level4 = [shuffle(primary_prompts, 4, 'and') for i in range(20000)]
+or_prompts_level2 = [shuffle(primary_prompts, 2, 'or') for i in range(20000)]
+or_prompts_level3 = [shuffle(primary_prompts, 3, 'or') for i in range(20000)]
+or_prompts_level4 = [shuffle(primary_prompts, 4, 'or') for i in range(20000)]
